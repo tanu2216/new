f2d48b861834db3db443ec62116cae80d3d9b853
tanu2216 <47083546+tanu2216@users.noreply.github.com>
2019-03-19 10:55:35 +0530

    Add files via upload

---
+++ HotelBooking/RunTestHotelBooking.java
@@ -0,0 +1,14 @@
+package HotelBooking;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin= {"pretty"})
+public class RunTestHotelBooking {
+	
+	
+
+}
+
